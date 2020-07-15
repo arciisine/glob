962d7ff1d78bc895e92bf45173f03d0854e038e8
Timothy Soehnlin <timothy.soehnlin@gmail.com>
2020-07-15 09:52:54 -0400

    Working through basic patterns

--- src/index.js
+++ src/index.js
@@ -15,18 +15,19 @@ function makeRe(input) {
   /** @type {number[]} */ const states = [];
 
   /** 
-   * @param {number} tk
+   * @param {number|number[]} tk
    * @param {number} state
    */
   const onToken = (ch, tk, state = 0) => {
-    if (tk) {
+    if (tk && !Array.isArray(tk)) {
       tokens.push(tk);
-      parts.push(...TOKEN_TO_RE[tk]);
-    } else if (ch in CHAR_TO_RE) {
-      parts.push(...CHAR_TO_RE[ch]);
-    } else {
-      parts.push(ch);
     }
+
+    parts.push(...(Array.isArray(tk) ? tk :
+      tk ? TOKEN_TO_RE[tk] :
+        (CHAR_TO_RE[ch] || [ch]))
+    );
+
     if (state > 0) {
       states.unshift(state);
     } else if (state < 0 && -state !== states.shift()) {
@@ -40,38 +41,82 @@ function makeRe(input) {
     const state = states[0];
     const prev = tokens[tokens.length - 1];
 
+    // State check
     switch (state) {
       case STATE.ESCAPED: onToken(ch, 0, -STATE.ESCAPED); continue;
       case STATE.RANGE: {
-        if (ch === CH.LEFT_SQUARE) { // Character class handling
-          let start = i += 1;
-          while (input.charCodeAt(i) !== CH.RIGHT_SQUARE) { i += 1; }
-          const key = input.substring(start, i);
-          if (!(key in POSIX_CLS)) {
-            throw new Error(`Unexpected character class: ${key}`)
+        switch (ch) {
+          case CH.HYPHEN: onToken(ch, TOKEN.RANGE_SEP); continue;
+          case CH.EXCLAMATION:
+          case CH.CARET:
+            if (prev === TOKEN.RANGE_START) {
+              onToken(ch, TOKEN.RANGE_NEGATE);
+              continue;
+            }
+            break;
+          case CH.LEFT_SQUARE: { // Character class handling
+            let start = i += 1;
+            while (input.charCodeAt(i) !== CH.RIGHT_SQUARE) { i += 1; }
+            const key = input.substring(start, i);
+            if (!(key in POSIX_CLS)) {
+              throw new Error(`Unexpected character class: ${key}`)
+            }
+            onToken(0, POSIX_CLS[key]);
+            continue;
           }
-          onToken(0, POSIX_CLS[key]);
-          continue;
+          case CH.PERIOD: case CH.QUESTION:
+          case CH.LEFT_CURLY: case CH.RIGHT_CURLY:
+          case CH.LEFT_PARENS: case CH.RIGHT_PARENS:
+          case CH.PLUS: case CH.ASTERISK:
+          case CH.BACKSLASH:
+            onToken(ch, [ch]);
+            continue;
         }
+        break;
       }
-      default:
-        if (nextCh === CH.LEFT_PARENS && ch in GROUP_CH_TO_TOKEN) {
-          onToken(ch, ...GROUP_CH_TO_TOKEN[ch]);
-          i += 1;
-          continue;
+      case STATE.CHOICE:
+        switch (ch) {
+          case CH.COMMA: onToken(ch, TOKEN.CHOICE_SEP); continue;
         }
+        break;
+      case STATE.GROUP_RE:
+        switch (ch) {
+          case CH.BAR: onToken(ch, TOKEN.CHOICE_SEP); continue;
+        }
+        break;
     }
 
+    // Check for special grouping symbols that may need to look back or forward
+    if (ch in GROUP_CH_TO_TOKEN) {
+      if (nextCh === CH.LEFT_PARENS) {
+        // Starting a glob group
+        onToken(ch, ...GROUP_CH_TO_TOKEN[ch]);
+        i += 1;
+        continue;
+      } else if (prev === TOKEN.GROUP_RE_END || prev === TOKEN.RANGE_END) {
+        switch (ch) { // Match RE groupings
+          case CH.PLUS:
+            onToken(ch, [ch]);
+            continue;
+        }
+      }
+    }
+
+    // Regular handling
     switch (ch) {
-      case CH.BACKSLASH: states.unshift(STATE.ESCAPED); break;
-      case CH.QUESTION: onToken(ch, state !== STATE.RANGE ? TOKEN.WILDCARD_ONE : 0); break;
-      case CH.EXCLAMATION: onToken(ch, state === STATE.RANGE && prev === TOKEN.RANGE_START ? TOKEN.RANGE_NEGATE : 0); break;
-      case CH.LEFT_CURLY: onToken(ch, TOKEN.CHOICE_START, STATE.CHOICE); break;
-      case CH.RIGHT_CURLY: onToken(ch, TOKEN.CHOICE_END, -STATE.CHOICE); break;
-      case CH.LEFT_SQUARE: onToken(ch, TOKEN.RANGE_START, STATE.RANGE); break;
-      case CH.RIGHT_SQUARE: onToken(ch, TOKEN.RANGE_END, -STATE.RANGE); break;
-      case CH.HYPHEN: onToken(ch, state === STATE.RANGE ? TOKEN.RANGE_SEP : 0); break;
-      case CH.COMMA: onToken(ch, state === STATE.CHOICE ? TOKEN.CHOICE_SEP : 0); break;
+      case CH.BACKSLASH: states.unshift(STATE.ESCAPED); continue;
+      case CH.LEFT_CURLY: onToken(ch, TOKEN.CHOICE_START, STATE.CHOICE); continue;
+      case CH.RIGHT_CURLY: onToken(ch, TOKEN.CHOICE_END, -STATE.CHOICE); continue;
+      case CH.LEFT_SQUARE: onToken(ch, TOKEN.RANGE_START, STATE.RANGE); continue;
+      case CH.RIGHT_SQUARE: onToken(ch, TOKEN.RANGE_END, -STATE.RANGE); continue;
+      case CH.LEFT_PARENS: onToken(ch, TOKEN.GROUP_RE_START, STATE.GROUP_RE); continue;
+      case CH.RIGHT_PARENS:
+        if (!(state in GROUP_STATE_TO_TOKEN)) {
+          throw new Error(`Unexecpted parens: )`)
+        }
+        onToken(ch, GROUP_STATE_TO_TOKEN[state], -state);
+        continue;
+      case CH.QUESTION: onToken(ch, TOKEN.WILDCARD_ONE); continue;
       case CH.ASTERISK:
         if (nextCh === CH.ASTERISK && (
           input.charCodeAt(i + 2) === CH.SLASH || (i + 2 === input.length))
@@ -81,24 +126,9 @@ function makeRe(input) {
         } else {
           onToken(ch, TOKEN.WILDCARD_ANY);
         }
-        break;
-      case CH.LEFT_PARENS:
-        if (state === STATE.RANGE) {
-          onToken(ch, 0);
-        } else {
-          onToken(ch, TOKEN.GROUP_ONE_START, STATE.GROUP_ONE);
-        }
-        break;
-      case CH.RIGHT_PARENS: {
-        if (!(state in GROUP_STATE_TO_TOKEN)) {
-          throw new Error(`Unexecpted parens: )`)
-        } else {
-          onToken(ch, GROUP_STATE_TO_TOKEN[state], -state);
-        }
-        break;
-      }
-      default: onToken(ch, 0);
+        continue;
     }
+    onToken(ch, 0);
   }
   return new RegExp(String.fromCharCode(CH.CARET, ...parts, CH.DOLLAR));
 }
