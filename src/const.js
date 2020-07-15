632f913c70026310a340ca865b3f4bb9d9f6015f
Timothy Soehnlin <timothy.soehnlin@gmail.com>
2020-07-14 23:54:50 -0400

    Setup

--- src/const.js
+++ src/const.js
@@ -22,7 +22,7 @@ const TOKEN = {
 };
 
 const STATE = {
-  ESCAPED: 0, RANGE: 1, CHOICE: 2, GROUP_ONE: 3, GROUP_ANY: 4, GROUP_MANY: 5, GROUP_OPT: 6, GROUP_NOT: 7
+  ESCAPED: 1, RANGE: 2, CHOICE: 3, GROUP_ONE: 4, GROUP_ANY: 5, GROUP_MANY: 6, GROUP_OPT: 7, GROUP_NOT: 8
 };
 
 /** @type {Record<number, [number,number]>}*/
@@ -55,20 +55,23 @@ const POSIX_CLS = {
 const TOKEN_TO_RE = {
   [TOKEN.WILDCARD_NESTED]: nums('([^/]*[/])*[^/]*'),
   [TOKEN.WILDCARD_ONE]: nums('[^/]'), [TOKEN.WILDCARD_ANY]: nums('[^/]*'),
-  [TOKEN.RANGE_NEGATE]: nums('^'), [TOKEN.CHOICE_START]: nums('('),
-  [TOKEN.CHOICE_SEP]: nums('|'), [TOKEN.CHOICE_END]: nums(')'),
+
+  [TOKEN.CHOICE_START]: nums('('), [TOKEN.CHOICE_SEP]: nums('|'), [TOKEN.CHOICE_END]: nums(')'),
+
   [TOKEN.RANGE_START]: nums('['), [TOKEN.RANGE_SEP]: nums('-'),
-  [TOKEN.RANGE_END]: nums(']'),
+  [TOKEN.RANGE_END]: nums(']'), [TOKEN.RANGE_NEGATE]: nums('^'),
+
   [TOKEN.GROUP_ONE_START]: nums('('), [TOKEN.GROUP_ONE_END]: nums(')'),
   [TOKEN.GROUP_ANY_START]: nums('('), [TOKEN.GROUP_ANY_END]: nums(')*'),
   [TOKEN.GROUP_MANY_START]: nums('('), [TOKEN.GROUP_MANY_END]: nums(')+'),
   [TOKEN.GROUP_OPT_START]: nums('('), [TOKEN.GROUP_OPT_END]: nums(')?'),
-  [TOKEN.GROUP_NOT_START]: nums('(?!'), [TOKEN.GROUP_NOT_END]: nums(')'),
-  [TOKEN.POSIX_ALNUM]: nums('a-zA-Z0-9'), [TOKEN.POSIX_ALPHA]: nums('[a-zA-Z]'),
-  [TOKEN.POSIX_ASCII]: nums('\\x00-\\x7f'), [TOKEN.POSIX_BLANK]: nums('[ \\t]'),
-  [TOKEN.POSIX_CNTRL]: nums('\\x00-\\x1f\\x7f'), [TOKEN.POSIX_DIGIT]: nums('[0-9]'),
+  [TOKEN.GROUP_NOT_START]: nums('(?!'), [TOKEN.GROUP_NOT_END]: nums(').*'),
+
+  [TOKEN.POSIX_ALNUM]: nums('a-zA-Z0-9'), [TOKEN.POSIX_ALPHA]: nums('a-zA-Z'),
+  [TOKEN.POSIX_ASCII]: nums('\\x00-\\x7f'), [TOKEN.POSIX_BLANK]: nums(' \\t'),
+  [TOKEN.POSIX_CNTRL]: nums('\\x00-\\x1f\\x7f'), [TOKEN.POSIX_DIGIT]: nums('0-9'),
   [TOKEN.POSIX_GRAPH]: nums('\\x21-\\x7e'), [TOKEN.POSIX_LOWER]: nums('a-z'),
-  [TOKEN.POSIX_PRINT]: nums('\\x20-\\x7e'), [TOKEN.POSIX_PUNCT]: nums('\\-!"#$%&\'()\\*+,./:;<=>?@\\\\^_{|}~'),
+  [TOKEN.POSIX_PRINT]: nums('\\x20-\\x7e'), [TOKEN.POSIX_PUNCT]: nums('\\-!"#$%&\'()\\*+,./:;<=>?@\\^_{|}~'),
   [TOKEN.POSIX_SPACE]: nums(' \\t\\r\\n\\v\\f'), [TOKEN.POSIX_UPPER]: nums('A-Z'),
   [TOKEN.POSIX_WORD]: nums('A-Za-z0-9_'), [TOKEN.POSIX_XDIGIT]: nums('A-Fa-f0-9')
 };
@@ -78,7 +81,7 @@ const CHAR_TO_RE = {
   [CH.LEFT_CURLY]: esc('{'), [CH.RIGHT_CURLY]: esc('}'),
   [CH.LEFT_SQUARE]: esc('['), [CH.RIGHT_SQUARE]: esc(']'),
   [CH.LEFT_PARENS]: esc('('), [CH.RIGHT_PARENS]: esc(')'),
-  [CH.PLUS]: esc('+'),
+  [CH.PLUS]: esc('+'), [CH.ASTERISK]: esc('*')
 }
 
 module.exports = {
--- src/index.js
+++ src/index.js
@@ -21,7 +21,7 @@ function makeRe(input) {
   const onToken = (ch, tk, state = 0) => {
     if (tk) {
       tokens.push(tk);
-      parts.push(...(TOKEN_TO_RE[tk] || CHAR_TO_RE[ch] || [ch]));
+      parts.push(...TOKEN_TO_RE[tk]);
     } else if (ch in CHAR_TO_RE) {
       parts.push(...CHAR_TO_RE[ch]);
     } else {
@@ -42,9 +42,20 @@ function makeRe(input) {
 
     switch (state) {
       case STATE.ESCAPED: onToken(ch, 0, -STATE.ESCAPED); continue;
-      case STATE.RANGE: break;
+      case STATE.RANGE: {
+        if (ch === CH.LEFT_SQUARE) { // Character class handling
+          let start = i = i + 1;
+          while (input.charCodeAt(i) !== CH.RIGHT_SQUARE) { i += 1; }
+          const key = input.substring(start, i);
+          if (!(key in POSIX_CLS)) {
+            throw new Error(`Unexpected character class: ${key}`)
+          }
+          onToken(0, POSIX_CLS[key]);
+          continue;
+        }
+      }
       default:
-        if (nextCh === CH.LEFT_PARENS && GROUP_CH_TO_TOKEN[ch]) {
+        if (nextCh === CH.LEFT_PARENS && ch in GROUP_CH_TO_TOKEN) {
           onToken(ch, ...GROUP_CH_TO_TOKEN[ch]);
           i += 1;
           continue;
@@ -52,28 +63,12 @@ function makeRe(input) {
     }
 
     switch (ch) {
-      case CH.BACKSLASH: states.push(STATE.ESCAPED); break;
+      case CH.BACKSLASH: states.unshift(STATE.ESCAPED); break;
       case CH.QUESTION: onToken(ch, state !== STATE.RANGE ? TOKEN.WILDCARD_ONE : 0); break;
       case CH.EXCLAMATION: onToken(ch, state === STATE.RANGE && prev === TOKEN.RANGE_START ? TOKEN.RANGE_NEGATE : 0); break;
       case CH.LEFT_CURLY: onToken(ch, TOKEN.CHOICE_START, STATE.CHOICE); break;
       case CH.RIGHT_CURLY: onToken(ch, TOKEN.CHOICE_END, -STATE.CHOICE); break;
-      case CH.LEFT_SQUARE: {
-        onToken(ch, TOKEN.RANGE_START, STATE.RANGE);
-        if (nextCh === CH.LEFT_SQUARE) { // Character class handling
-          let start = i = i + 2;
-          while (input.charCodeAt(i) !== CH.RIGHT_SQUARE) { i += 1; }
-          const key = input.substring(start, i);
-          if (!(key in POSIX_CLS)) {
-            throw new Error(`Unexpected character class: ${key}`)
-          }
-          onToken(0, POSIX_CLS[key]);
-          onToken(input.charCodeAt(i), TOKEN.RANGE_END, -STATE.RANGE);
-          if (input.charCodeAt(i += 1) !== CH.RIGHT_SQUARE) {
-            throw new Error(`Unexpected character: ${input.charAt(i)}, expected ']'`);
-          }
-        }
-        break;
-      }
+      case CH.LEFT_SQUARE: onToken(ch, TOKEN.RANGE_START, STATE.RANGE); break;
       case CH.RIGHT_SQUARE: onToken(ch, TOKEN.RANGE_END, -STATE.RANGE); break;
       case CH.HYPHEN: onToken(ch, state === STATE.RANGE ? TOKEN.RANGE_SEP : 0); break;
       case CH.COMMA: onToken(ch, state === STATE.CHOICE ? TOKEN.CHOICE_SEP : 0); break;
@@ -83,9 +78,10 @@ function makeRe(input) {
         break;
       case CH.RIGHT_PARENS: {
         if (!(state in GROUP_STATE_TO_TOKEN)) {
-          throw new Error(`'Unexpected character: ${input.charAt(i)}`);
+          onToken(ch, 0)
+        } else {
+          onToken(ch, GROUP_STATE_TO_TOKEN[state], -state);
         }
-        onToken(ch, GROUP_STATE_TO_TOKEN[state], -state);
         break;
       }
       default: onToken(ch, 0);
